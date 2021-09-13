46217295d454d43acc1e34c4d85858d7238e217a
Ashish Verma <ashish.verma@gartner.com>
2021-09-13 18:08:51 +0530

    changed relative path

--- build/App.js
+++ build/App.js
@@ -10,7 +10,7 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var App = function App() {
-  return /*#__PURE__*/_react["default"].createElement("div", null, "This is Hi v2.0.0");
+  return /*#__PURE__*/_react["default"].createElement("div", null, "This is Hi v3.0.0");
 };
 
 var _default = App;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <div>This is Hi v2.0.0</div>
+  <div>This is Hi v3.0.0</div>
 );
 
 export default App;
